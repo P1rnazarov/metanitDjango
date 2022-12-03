f69ec3ece8a7ee6903ea7705f8ae739e60fcb977
P1rnazarov <otabek200101@gmail.com>
2022-12-03 10:17:04 +0500

    Send Data to Html with Django Lists

--- metanit/urls.py
+++ metanit/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path, include
 from myone import views
 
-product_patterns = [
-    path("", views.products),
-    path("comments", views.comments),
-    path("questions", views.questions),
-]
-
 urlpatterns = [
     path("", views.index),
-    path("user/", views.user),
-    path("products/<int:id>/", include(product_patterns)),
+    path("send", views.send),
 ]
--- myone/views.py
+++ myone/views.py
@@ -3,24 +3,15 @@ from django.shortcuts import render
 from django.template.response import TemplateResponse
 
 
-def products(request, id):
-    return HttpResponse(f"Товар {id}")
-
-
-def comments(request, id):
-    return HttpResponse(f"Комментарии о товаре {id}")
-
-
-def questions(request, id):
-    return HttpResponse(f"Вопросы о товаре {id}")
-
-
 def index(request):
-    data = {"header":"Metanit", "message":"My first message to Django"}
-    return TemplateResponse(request,  "index.html", context=data)
+    data = {"header": "Metanit", "message": "My first message to Django"}
+    return TemplateResponse(request, "index.html", context=data)
+def send(request):
+    header = "Данные пользователя"  # обычная переменная
+    langs = ["Python", "Java", "C#"]  # список
+    user = {"name": "Tom", "age": 23}  # словарь
+    address = ("Абрикосовая", 23, 45)  # кортеж
 
+    data = {"header": header, "langs": langs, "user": user, "address": address}
 
-def user(request):
-    age = request.GET.get("age")
-    name = request.GET.get("name")
-    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
+    return TemplateResponse(request, "index_2.html", context=data)
