f4cd5d49a5d2bd313d6ad63d5182b22347e644a4
P1rnazarov <otabek200101@gmail.com>
2022-12-03 09:47:56 +0500

    Initial commit

---
+++ myone/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from django.template.response import TemplateResponse
+
+
+def products(request, id):
+    return HttpResponse(f"Товар {id}")
+
+
+def comments(request, id):
+    return HttpResponse(f"Комментарии о товаре {id}")
+
+
+def questions(request, id):
+    return HttpResponse(f"Вопросы о товаре {id}")
+
+
+def index(request):
+    return TemplateResponse(request,  "index.html")
+
+
+def user(request):
+    age = request.GET.get("age")
+    name = request.GET.get("name")
+    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
